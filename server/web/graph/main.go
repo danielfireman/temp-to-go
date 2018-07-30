c3c7ce91b1d0355dabbe87da8f5303b64df555e7
Daniel Fireman <danielfireman@gmail.com>
2018-07-30 15:12:38 -0300

    Another improvement to JS size: bye bye fmtless.

--- server/web/graph/main.go
+++ server/web/graph/main.go
@@ -3,7 +3,6 @@ package main
 //go:generate gopherjs build main.go -m -o ../public/js/graph.js
 
 import (
-	"github.com/cathalgarvey/fmtless/encoding/json"
 	charts "github.com/cnguy/gopherjs-frappe-charts"
 	"github.com/gopherjs/gopherjs/js"
 
@@ -11,8 +10,9 @@ import (
 )
 
 type weatherResponse struct {
-	Hour []string  `json:"hour,omitempty"`
-	Temp []float64 `json:"temp,omitempty"`
+	*js.Object
+	Hour []string  `js:"hour"`
+	Temp []float64 `js:"temp"`
 }
 
 const timezoneHeader = "TZ"
@@ -24,27 +24,17 @@ func main() {
 	// Issuing the request.
 	req := xhr.NewRequest("GET", "/restricted/weather")
 	req.Timeout = 5000
-	req.ResponseType = xhr.ArrayBuffer
+	req.SetRequestHeader("Content-Type", "application/json")
 	req.SetRequestHeader("TZ", tz)
+	req.ResponseType = xhr.JSON
 	if err := req.Send(tz); err != nil {
 		println(err)
 		return
 	}
-	if req.Status != 200 {
-		println("Not 200")
-		return
-	}
-	b := js.Global.Get("Uint8Array").New(req.Response).Interface().([]byte)
-	println(string(b))
-	var wr weatherResponse
-	if err := json.Unmarshal(b, &wr); err != nil {
-		println(err)
-		return
-	}
+	wr := &weatherResponse{Object: req.Response}
 	chartData := charts.NewChartData()
 	chartData.Labels = wr.Hour
-	chartData.SpecificValues = []*charts.SpecificValue{charts.NewSpecificValue("", "solid", 0)}
-	// Workaround to set the minimum value: https://github.com/frappe/charts/issues/86
+	chartData.SpecificValues = []*charts.SpecificValue{charts.NewSpecificValue("", "solid", 0)} // Workaround to set the minimum value: https://github.com/frappe/charts/issues/86
 	chartData.Datasets = []*charts.Dataset{
 		charts.NewDataset(
 			"Temperature (Celsius)",
