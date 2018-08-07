f5bd6dc7c615c9a18d5b519d7fccb6e3c6ac3aef
Daniel Fireman <danielfireman@gmail.com>
2018-08-07 14:52:29 -0300

    Fix predictor worker and add integration test

--- server/worker/predictor/main.go
+++ server/worker/predictor/main.go
@@ -31,51 +31,40 @@ func main() {
 	st := time.Now().Add(-7 * 24 * time.Hour)
 	et := time.Now()
 
-	var r regression.Regression
-	r.SetObserved("Bedroom temperature")
-	r.SetVar(0, "Weather Temperature")
-	r.SetVar(1, "Fan State")
-	r.SetVar(2, "Timestamp")
-
-	// ### Trainning.
-	bs, err := bedroomService.FetchState(st, et)
-	if err != nil {
-		log.Fatalf("Error fetching past bedroom temperature: %q", err)
-	}
-	if len(bs) == 0 {
-		log.Fatalf("Can not predict without any bedroom temperature.")
-	}
-
-	// Normalize the start and end timestamp (to match the bedroom ones)
-	st, et = bs[0].Timestamp, bs[len(bs)-1].Timestamp
-	ws, err := weatherService.Fetch(st, et)
-	if err != nil {
-		log.Fatalf("Error fetching past weather: %q", err)
-	}
-	wsMap := make(map[time.Time]float64)
-	for _, s := range ws {
-		wsMap[s.Timestamp] = s.Temp
-	}
-	fs, err := fanService.FetchState(st, et)
-	if err != nil {
-		log.Fatalf("Error fetching fan state: %q", err)
-	}
-	fsMap := fillFanState(fs, st, et)
-	var trainSet regression.DataPoints
-	for _, b := range bs {
-		t := b.Timestamp
-		f, fok := fsMap[t]
-		w, wok := wsMap[t]
-		if fok && wok {
-			trainSet = append(trainSet, regression.DataPoint(b.Temperature, []float64{w, f, float64(b.Timestamp.Unix())}))
-		}
-	}
-	r.Train(trainSet...)
+	r := newRegression()
+	r.Train(getTrainSet(st, et, weatherService, fanService, bedroomService)...)
 
 	// Run model.
 	r.Run()
 
 	// Predict the next 24 hours and updates the database.
+	predictions := predict(r, et, forecastService)
+	if err := predictionService.Update(predictions...); err != nil {
+		log.Fatalf("Error updating predictions:%q", err)
+	}
+}
+
+func newRegression() regression.Regression {
+	var r regression.Regression
+	r.SetObserved("Bedroom temperature")
+	r.SetVar(0, "Weather Temperature")
+	r.SetVar(1, "Fan State")
+	return r
+}
+
+func predInputs(ws, fs float64) []float64 {
+	return []float64{ws, fs}
+}
+
+func predOrDie(r regression.Regression, w, f float64) float64 {
+	p, err := r.Predict(predInputs(w, f))
+	if err != nil {
+		log.Fatalf("Error predicting temperature with fan:%.0f weather:%f :%q", f, w, err)
+	}
+	return p
+}
+
+func predict(r regression.Regression, et time.Time, forecastService *tsmongo.ForecastService) []tsmongo.Prediction {
 	forecast, err := forecastService.Fetch(et, et.Add(24*time.Hour))
 	if err != nil {
 		log.Fatal(err)
@@ -88,17 +77,41 @@ func main() {
 			TempFanHigh: predOrDie(r, f.Temp, 2),
 		})
 	}
-	if err := predictionService.Update(predictions...); err != nil {
-		log.Fatalf("Error updating predictions:%q", err)
-	}
+	return predictions
 }
 
-func predOrDie(r regression.Regression, temp, fanMode float64) float64 {
-	p, err := r.Predict([]float64{temp, fanMode})
+func getTrainSet(st, et time.Time, weatherService *tsmongo.WeatherService, fanService *tsmongo.FanService, bedroomService *tsmongo.BedroomService) regression.DataPoints {
+	bs, err := bedroomService.FetchState(st, et)
 	if err != nil {
-		log.Fatalf("Error predicting temperature with fan %.0f:%q", fanMode, err)
+		log.Fatalf("Error fetching past bedroom temperature: %q", err)
 	}
-	return p
+	if len(bs) == 0 {
+		log.Fatalf("Can not predict without any bedroom temperature.")
+	}
+	ws, err := weatherService.Fetch(st, et)
+	if err != nil {
+		log.Fatalf("Error fetching past weather: %q", err)
+	}
+	fs, err := fanService.FetchState(st, et)
+	if err != nil {
+		log.Fatalf("Error fetching fan state: %q", err)
+	}
+	wsMap := make(map[time.Time]float64)
+	for _, s := range ws {
+		wsMap[s.Timestamp] = s.Temp
+	}
+	fsMap := fillFanState(fs, st, et)
+	var trainSet regression.DataPoints
+	for _, b := range bs {
+		// Only consider valid for the train set there is a full tuple.
+		t := b.Timestamp
+		f, fok := fsMap[t]
+		w, wok := wsMap[t]
+		if fok && wok {
+			trainSet = append(trainSet, regression.DataPoint(b.Temperature, predInputs(w, f)))
+		}
+	}
+	return trainSet
 }
 
 func fillFanState(fs []tsmongo.FanState, st, et time.Time) map[time.Time]float64 {
--- server/worker/predictor/main_test.go
+++ server/worker/predictor/main_test.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/danielfireman/temp-to-go/server/tsmongo"
-
+	"github.com/danielfireman/temp-to-go/server/weather"
 	"github.com/globalsign/mgo/dbtest"
 )
 
@@ -14,16 +15,82 @@ var mongoDB dbtest.DBServer
 
 func TestPredictor(t *testing.T) {
 	tempDir, _ := ioutil.TempDir("", "predictor_testing")
+	defer func() { os.RemoveAll(tempDir) }()
+
 	mongoDB.SetPath(tempDir)
-	// Wipe already closes all sessions.
-	defer func() { mongoDB.Wipe() }()
+	defer mongoDB.Stop()
+	defer mongoDB.Wipe()
 
-	fmt.Println("Before mgo session")
-	_ = mongoDB.Session()
-	fmt.Println("After")
+	s := mongoDB.Session()
+	defer s.Close()
+	dbNames, _ := s.DatabaseNames()
+	dbName := dbNames[0]
 
-	_, err := tsmongo.Dial("localhost:27017")
-	if err != nil {
-		t.Error(err)
+	session := tsmongo.NewSession(s, dbName)
+	defer session.Close()
+
+	startTime := time.Now()
+	endTime := startTime.Add(12 * time.Hour)
+
+	bs := tsmongo.NewBedroomService(session)
+	bs.UpdateTemperature(startTime.Add(1*time.Hour), 27)
+	bs.UpdateTemperature(startTime.Add(2*time.Hour), 28)
+	bs.UpdateTemperature(startTime.Add(3*time.Hour), 25) // User switches the fan on (high).
+	bs.UpdateTemperature(startTime.Add(4*time.Hour), 25)
+	bs.UpdateTemperature(startTime.Add(5*time.Hour), 27) // User switches the fan off.
+	bs.UpdateTemperature(startTime.Add(6*time.Hour), 28.9)
+	bs.UpdateTemperature(startTime.Add(7*time.Hour), 30)
+	bs.UpdateTemperature(startTime.Add(8*time.Hour), 31)
+	bs.UpdateTemperature(startTime.Add(9*time.Hour), 27) // User switches the fan on (low).
+	bs.UpdateTemperature(startTime.Add(10*time.Hour), 26.5)
+	bs.UpdateTemperature(startTime.Add(11*time.Hour), 26)
+	bs.UpdateTemperature(startTime.Add(12*time.Hour), 27) // User switches the fan off.
+
+	fs := tsmongo.NewFanService(session)
+	fs.UpdateStatus(startTime.Add(3*time.Hour), tsmongo.FanHighSpeed)
+	fs.UpdateStatus(startTime.Add(5*time.Hour), tsmongo.FanOff)
+	fs.UpdateStatus(startTime.Add(9*time.Hour), tsmongo.FanLowSpeed)
+	fs.UpdateStatus(startTime.Add(12*time.Hour), tsmongo.FanOff)
+
+	// The temperature outside changes very little during the considered period.
+	ws := tsmongo.NewWeatherService(session)
+	ws.Update(weather.State{Timestamp: startTime.Add(1 * time.Hour), Temp: 27})
+	ws.Update(weather.State{Timestamp: startTime.Add(2 * time.Hour), Temp: 27.5})
+	ws.Update(weather.State{Timestamp: startTime.Add(3 * time.Hour), Temp: 28})
+	ws.Update(weather.State{Timestamp: startTime.Add(4 * time.Hour), Temp: 29})
+	ws.Update(weather.State{Timestamp: startTime.Add(5 * time.Hour), Temp: 28.8})
+	ws.Update(weather.State{Timestamp: startTime.Add(6 * time.Hour), Temp: 28.6})
+	ws.Update(weather.State{Timestamp: startTime.Add(7 * time.Hour), Temp: 29})
+	ws.Update(weather.State{Timestamp: startTime.Add(8 * time.Hour), Temp: 30})
+	ws.Update(weather.State{Timestamp: startTime.Add(9 * time.Hour), Temp: 29})
+	ws.Update(weather.State{Timestamp: startTime.Add(10 * time.Hour), Temp: 28})
+	ws.Update(weather.State{Timestamp: startTime.Add(11 * time.Hour), Temp: 27})
+	ws.Update(weather.State{Timestamp: startTime.Add(12 * time.Hour), Temp: 27.5})
+
+	// The current forecast source (OWM) only provides foreacasts for every 3 hours.
+	// We are simulating like the evening forecast, with temperatures dropping.
+	forecasts := []weather.State{
+		weather.State{Timestamp: endTime.Add(3 * time.Hour), Temp: 26},
+		weather.State{Timestamp: endTime.Add(6 * time.Hour), Temp: 25},
+		weather.State{Timestamp: endTime.Add(9 * time.Hour), Temp: 24},
+		weather.State{Timestamp: endTime.Add(12 * time.Hour), Temp: 23},
+		weather.State{Timestamp: endTime.Add(15 * time.Hour), Temp: 22},
+	}
+	fcs := tsmongo.NewForecastService(session)
+	fcs.Update(forecasts...)
+	r := newRegression()
+	r.Train(getTrainSet(startTime, endTime, ws, fs, bs)...)
+	r.Run()
+	predictions := predict(r, endTime, fcs)
+	if len(predictions) != len(forecasts) {
+		t.Errorf("len(predictions) want:%d got:%d", len(forecasts), len(predictions))
+	}
+	for _, p := range predictions {
+		if p.TempFanOff < p.TempFanLow {
+			t.Errorf("p.TempFanOff > p.TempFanLow p.TempFanOff:%f p.TempFanLow:%f", p.TempFanOff, p.TempFanLow)
+		}
+		if p.TempFanLow < p.TempFanHigh {
+			t.Errorf("p.TempFanOff > p.TempFanLow p.TempFanLow:%f p.TempFanHifh:%f", p.TempFanLow, p.TempFanHigh)
+		}
 	}
 }
