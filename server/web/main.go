4aef7a623dc02050aecfacedb9cf65ed07584824
lucasgomide <lucaslg200@gmail.com>
2018-10-02 14:54:37 -0300

    Register mandatory and non-mandatory env vars
    This commit closes https://github.com/danielfireman/temp-to-go/issues/21

--- server/web/main.go
+++ server/web/main.go
@@ -4,12 +4,12 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/danielfireman/temp-to-go/server/tsmongo"
 	"github.com/gorilla/sessions"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/middleware"
@@ -21,29 +21,37 @@ const (
 	restrictedPath = "/restricted"
 )
 
+// Specification represents a map of enviornment variables
+type Specification struct {
+	UserPassword  string `envconfig:"USERPASSWD" required:"true"`
+	EncryptionKey string `envconfig:"ENCRYPTION_KEY" required:"true"`
+
+	MongodbURI string `default:"mongodb://127.0.0.1:27017/db" envconfig:"MONGODB_URI"`
+	Port       string `default:"8080"`
+	PublicHTML string `envconfig:"PUBLIC_HTML" default:"public"`
+}
+
 func main() {
-	userPasswd := os.Getenv("USERPASSWD")
-	if userPasswd == "" {
-		log.Fatal("USERPASSWD must be set.")
+	var spec Specification
+	err := envconfig.Process("", &spec)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
-	key := []byte(os.Getenv("ENCRYPTION_KEY"))
+
+	key := []byte(spec.EncryptionKey)
 	if len(key) != 32 {
 		log.Fatalf("ENCRYPTION_KEY must be 32-bytes long. Current key is \"%s\" which is %d bytes long.", key, len(key))
 	}
-	mgoURI := os.Getenv("MONGODB_URI")
-	if mgoURI == "" {
-		log.Fatalf("Invalid MONGODB_URI: %s", mgoURI)
-	}
-	tsmongoSession, err := tsmongo.Dial(mgoURI)
+	tsmongoSession, err := tsmongo.Dial(spec.MongodbURI)
 	if err != nil {
-		log.Fatalf("Error connecting to StatusDB: %s", mgoURI)
+		log.Fatalf("Error connecting to StatusDB: %s", spec.MongodbURI)
 	}
 	defer tsmongoSession.Close()
 	fanService := tsmongo.NewFanService(tsmongoSession)
 	bedroomService := tsmongo.NewBedroomService(tsmongoSession)
 	weatherService := tsmongo.NewWeatherService(tsmongoSession)
 
-	publicHTML := filepath.Join(os.Getenv("PUBLIC_HTML"))
+	publicHTML := filepath.Join(spec.PublicHTML)
 
 	// Initializing web framework.
 	e := echo.New()
@@ -62,7 +70,7 @@ func main() {
 
 	// Public Routes.
 	bedroomAPIHandler := bedroomAPIHandler{key, bedroomService}
-	loginHandler := loginHandler{userPasswd}
+	loginHandler := loginHandler{spec.UserPassword}
 	e.File("/", filepath.Join(publicHTML, "index.html"))
 	e.File("/favicon.ico", filepath.Join(publicHTML, "favicon.ico"))
 	e.Static("/", publicHTML)
@@ -83,12 +91,9 @@ func main() {
 	restricted.GET("/indoortemp", bedroomAPIHandler.handleGet)
 
 	// Starting server.
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatalf("Invalid PORT: %s", port)
-	}
+
 	s := &http.Server{
-		Addr:         ":" + port,
+		Addr:         ":" + spec.Port,
 		ReadTimeout:  5 * time.Minute,
 		WriteTimeout: 5 * time.Minute,
 	}
